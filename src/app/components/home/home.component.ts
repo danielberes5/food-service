c324bd2c0d9a5d241f480140a26ed2b34cfb77be
danielberes5 <55472512+danielberes5@users.noreply.github.com>
2024-11-21 17:19:29 +0100

    Added Header and Home page

---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import {FoodService} from "../../services/food/food.service";
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent {
+  foods: String[] = [];
+
+  constructor(private foodService: FoodService) {
+
+  }
+
+  ngOnInit() {
+    this.foods = this.foodService.getAll()
+  }
+}
